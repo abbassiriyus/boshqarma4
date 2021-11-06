73edfa10349108246ef5d4b31c100e12faabf327
abbassiriyus <86982819+abbassiriyus@users.noreply.github.com>
2021-11-06 15:48:33 +0500

    wpeoiw
    ewsadsd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Dashboard from './pages/js/Dashboard'
+import Footer from './pages/js/Footer'
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Dashboard/>
+        <Footer/>
       </div>
     )
   }
--- src/pages/js/Dashboard.js
+++ src/pages/js/Dashboard.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Accordion, Container, Nav, Navbar } from 'react-bootstrap';
-import style from '../css/Dashboard.module.css'
+import { Accordion, Card, Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
+import style from '../css/Dashboard.module.css';
+
+
 export default class Dashboard extends Component {
   render() {
     return (
       <div>
-      <Navbar className={style.navbar} expand="lg"> 
+        {/* navbar */}
+      <Navbar className={style.navbar}  expand="lg"> 
   <Container>
-    <Navbar.Brand href="#home"><img className={style.imglogo} src="https://www.clipartmax.com/png/full/203-2035601_uzbekistans-emblem-uzbekistan-gerb-png.png"/> </Navbar.Brand>
+    <Navbar.Brand href="#home"><img alt=" " className={style.imglogo} src="https://www.clipartmax.com/png/full/203-2035601_uzbekistans-emblem-uzbekistan-gerb-png.png"/> </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav" >
       <Nav className="me-auto" id={style.Nav} >
@@ -22,22 +25,21 @@ export default class Dashboard extends Component {
     </Navbar.Collapse>
   </Container>
 </Navbar>
-<div className={style.asosiy}>
+
+{/* Asosiy qism */}
+<div id="home" className={style.asosiy}>
  
 <div className={style.rahbar}>
 
 
-  <div className={style.oyna}>
-<img src="https://telegra.ph/file/37c9140e82eba1db37cb4.jpg"  />
+        
+        <div className={style.oyna}>
+<img alt=" " src="https://telegra.ph/file/37c9140e82eba1db37cb4.jpg"  />
 <h1>Inson haddan tashqari ko‘p </h1>
 <h3>gapirsa, bu uning o‘ziga ishonmasligidan dalolat berishi ham mumkin. Yaxshi rahbar o‘z xodimining fikrini bayon qilishiga imkon beradi, chunki u uchun xodimlarining fikrlari haqiqatda qiziq. Rahbar ularni o‘ylashga undaydigan va ishning mohiyatini chuqurroq anglab yetishga yordam beradigan qiziq savollar beradi. Bu qanday tuzilgan? Sizga uning nimasi yoqdi? Bundan qanday saboq oldingiz? Bu ishni yanada yaxshi bajarishingiz uchun nima kerak bo‘ladi?</h3>
 
   </div>
-
-
-
-
-
+ 
 </div><div className={style.yangili}>
 <div style={{paddingBottom:'10px'}} className={style.yangilik}>
   <h1>Yangiliklar</h1>
@@ -98,13 +100,74 @@ export default class Dashboard extends Component {
 
 </div>
 </div>
+</div>
 
+{/* videolavhalar */}
 
+<div className={style.video}>
 
+  <div className={style.cardvideo}>
+    <h1 >Videolavhalar</h1>
+<Row >
+<Col lg={4} md={6} sm={12} className={style.col} >
 
-
+  <Card  >
+  <iframe className={style.ifre} title=" " 
+src="https://www.youtube.com/embed/Hz13r28Kc">
+</iframe> 
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+  </Card.Body>
+</Card>
+</Col>
+<Col lg={4} md={6} sm={12} className={style.col}>  <Card  >
+  <iframe className={style.ifre}  title=" " 
+src="https://www.youtube.com/embed/Hz13r28Kc">
+</iframe> 
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+  </Card.Body>
+</Card></Col>
+<Col lg={4} md={6} sm={12} className={style.col}>  <Card  >
+  <iframe className={style.ifre} title=" " 
+src="https://www.youtube.com/embed/Hz13r28Kc">
+</iframe> 
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+  </Card.Body>
+</Card></Col>
+<Col lg={4} md={6} sm={12} className={style.col}>  <Card  >
+  <iframe className={style.ifre} title=" " 
+src="https://www.youtube.com/embed/Hz13r28Kc">
+</iframe> 
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+  </Card.Body>
+</Card></Col>
+<Col lg={4} md={6} sm={12} className={style.col}>  <Card  >
+  <iframe className={style.ifre} title=" " 
+src="https://www.youtube.com/embed/Hz13r28Kc">
+</iframe> 
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+  </Card.Body>
+</Card></Col>
+<Col lg={4} md={6} sm={12} className={style.col}>  <Card  >
+  <iframe className={style.ifre} title=" " 
+src="https://www.youtube.com/embed/Hz13r28Kc">
+</iframe> 
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+  </Card.Body>
+</Card></Col>
+</Row>
+<h3>Barcha videolarni ko`rish</h3>
+</div>
 
 </div>
+
+
+
       </div>
     )
   }
