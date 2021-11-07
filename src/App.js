ddb7d2ec05f925ba4a62a82d18aa6b0836e462b4
Akbarshoh <93857309+intelligento3@users.noreply.github.com>
2021-11-07 15:11:43 +0500

    tayyor
    gyh

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
-import Dashboard from './pages/js/Dashboard'
-import Footer from './pages/js/Footer'
+
+import Tumanlar from './pages/js/Tumanlar'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Dashboard/>
-        <Footer/>
+       <Tumanlar/>
       </div>
     )
   }
