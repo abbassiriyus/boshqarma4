03daf9c48ff685e9431a0f8f300459d16eaa396a
Diyorbek5 <davlatovdiyorbek15@gmail.com>
2021-11-10 18:01:32 +0500

    Rahbariyat

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-
+import Dashboard from './pages/js/Dashboard';
 import Maktab from './pages/js/Maktab';
+import Rahbariyat from './pages/js/Rahbariyat';
 
 
 
@@ -9,8 +10,9 @@ export default class App extends Component {
   render() {
     return (
       <div>
-       {/* <Dashboard/> */}
-        <Maktab />
+       {/* <Dashboard /> */}
+        {/* <Maktab /> */}
+        <Rahbariyat />
       </div>
     )
   }
